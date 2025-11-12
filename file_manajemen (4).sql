cc220d431d9fc17332ccaea76d171d6410b6e72d
Divavania <divavaniacandrawati@gmail.com>
2025-11-12 19:44:01 +0700

    UPDATE: db

--- file_manajemen (4).sql
+++ file_manajemen (4).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 04 Nov 2025 pada 13.51
+-- Waktu pembuatan: 12 Nov 2025 pada 13.42
 -- Versi server: 10.4.32-MariaDB
 -- Versi PHP: 8.2.12
 
@@ -24,6 +24,34 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Struktur dari tabel `favorites`
+--
+
+CREATE TABLE `favorites` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `user_id` bigint(20) UNSIGNED NOT NULL,
+  `file_id` bigint(20) UNSIGNED DEFAULT NULL,
+  `folder_id` bigint(20) UNSIGNED DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `favorites`
+--
+
+INSERT INTO `favorites` (`id`, `user_id`, `file_id`, `folder_id`, `created_at`, `updated_at`) VALUES
+(1, 6, 35, NULL, '2025-11-08 00:05:27', '2025-11-08 00:05:27'),
+(17, 6, 37, NULL, '2025-11-08 08:47:18', '2025-11-08 08:47:18'),
+(18, 6, 38, NULL, '2025-11-08 10:14:45', '2025-11-08 10:14:45'),
+(19, 6, 23, NULL, '2025-11-08 10:14:58', '2025-11-08 10:14:58'),
+(21, 6, 40, NULL, '2025-11-08 10:52:25', '2025-11-08 10:52:25'),
+(22, 6, 98, NULL, '2025-11-11 01:14:59', '2025-11-11 01:14:59'),
+(23, 6, 102, NULL, '2025-11-12 05:35:33', '2025-11-12 05:35:33');
+
+-- --------------------------------------------------------
+
+--
 -- Struktur dari tabel `files`
 --
 
@@ -37,22 +65,23 @@ CREATE TABLE `files` (
   `file_type` varchar(255) DEFAULT NULL,
   `file_size` bigint(20) DEFAULT NULL,
   `mime_type` varchar(100) DEFAULT NULL,
-  `status` enum('Public','Private') DEFAULT 'Private',
+  `status` enum('Public','Private','Selective') DEFAULT 'Private',
   `description` text DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `deleted_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data untuk tabel `files`
 --
 
-INSERT INTO `files` (`id`, `folder_id`, `uploaded_by`, `divisi`, `file_name`, `file_path`, `file_type`, `file_size`, `mime_type`, `status`, `description`, `created_at`, `updated_at`) VALUES
-(20, NULL, 7, NULL, 'Siomay.jpg', 'uploads/files/FTx5jYm383CESptkCK8SzChHtIOupaOar0McEoTb.jpg', 'jpg', 54229, NULL, 'Private', NULL, '2025-11-04 05:30:13', '2025-11-04 05:30:13'),
-(21, NULL, 6, NULL, 'pulau bali.webp', 'uploads/files/UJceJX4ipSs4MoE279SYZXQxVK8aOOP36JP4hdKK.webp', 'webp', 32852, NULL, 'Private', NULL, '2025-11-04 05:44:33', '2025-11-04 05:44:33'),
-(22, NULL, 6, NULL, 'Minggu-11 25-29 Agustus.docx.pdf', 'uploads/files/zgMEMTyXcH3LIGfsJO9lwvaci0dNjewJz9obFMOF.pdf', 'pdf', 86881, NULL, 'Private', NULL, '2025-11-04 05:45:31', '2025-11-04 05:45:31'),
-(23, NULL, 6, NULL, 'Screen Recording 2025-10-21 193620.mp4', 'uploads/files/StM7DFa49KuzbS5OhR52Lj9i6CTwUKrZYWhsEe5W.mp4', 'mp4', 1594244, NULL, 'Private', NULL, '2025-11-04 05:46:02', '2025-11-04 05:46:02'),
-(24, NULL, 6, NULL, 'Caption feed.txt', 'uploads/files/ppEllCM4ZDnMVBYyjf7CDPvr9IUOReQQCB9cZEbj.txt', 'txt', 748, NULL, 'Private', NULL, '2025-11-04 05:46:44', '2025-11-04 05:46:44');
+INSERT INTO `files` (`id`, `folder_id`, `uploaded_by`, `divisi`, `file_name`, `file_path`, `file_type`, `file_size`, `mime_type`, `status`, `description`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(81, NULL, 6, NULL, 'Hasil edit foto berada di tengah salju lembut dengan jaket puffy pastel.png', 'uploads/files/aRyegYTdhpyd4ElNNPvDVkKKNFsF2MqfOhxA8oMq.png', 'png', 1115755, NULL, 'Private', NULL, '2025-11-10 23:53:50', '2025-11-12 05:01:38', '2025-11-12 05:01:38'),
+(99, NULL, 6, NULL, 'WhatsApp Image 2025-11-12 at 10.57.53.jpeg', 'uploads/files/Z3AI9ONLt1vOQtntiu9o8ExcCgTXAJwrx6GeXJJz.jpg', 'jpeg', 195639, NULL, 'Private', NULL, '2025-11-12 05:00:25', '2025-11-12 05:02:14', '2025-11-12 05:02:14'),
+(100, 45, 6, NULL, 'Hasil edit foto santai tepi danau.png', 'uploads/Hasil edit foto santai tepi danau.png', 'png', NULL, NULL, 'Private', NULL, '2025-11-12 05:15:11', '2025-11-12 05:35:31', '2025-11-12 05:35:31'),
+(101, 46, 6, NULL, 'Screen Recording 2025-11-09 174325.mp4', 'uploads/Screen Recording 2025-11-09 174325.mp4', 'mp4', NULL, NULL, 'Private', NULL, '2025-11-12 05:34:15', '2025-11-12 05:34:15', NULL),
+(102, 47, 6, NULL, 'Hasil edit foto night street style.png', 'uploads/Hasil edit foto night street style.png', 'png', NULL, NULL, 'Private', NULL, '2025-11-12 05:34:15', '2025-11-12 05:34:15', NULL);
 
 -- --------------------------------------------------------
 
@@ -97,21 +126,55 @@ CREATE TABLE `folders` (
   `created_by` int(11) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `deleted_at` timestamp NULL DEFAULT NULL
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  `sort_order` int(11) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data untuk tabel `folders`
 --
 
-INSERT INTO `folders` (`id`, `name`, `parent_id`, `divisi`, `status`, `created_by`, `created_at`, `updated_at`, `deleted_at`) VALUES
-(17, 'Laravel', NULL, NULL, 'Private', 6, '2025-11-02 05:46:48', '2025-11-02 05:46:48', NULL),
-(18, 'Contoh', 17, NULL, 'Private', 6, '2025-11-02 05:46:59', '2025-11-02 05:58:04', NULL),
-(19, 'Coba', 18, NULL, 'Private', 6, '2025-11-02 05:47:15', '2025-11-02 05:47:15', NULL),
-(20, 'Coba', NULL, NULL, 'Private', 6, '2025-11-04 01:37:29', '2025-11-04 05:40:11', NULL),
-(21, 'POLTEK', NULL, NULL, 'Private', 7, '2025-11-04 01:39:55', '2025-11-04 01:39:55', NULL),
-(22, 'TEKNIK', NULL, NULL, 'Private', 7, '2025-11-04 01:40:23', '2025-11-04 01:40:23', NULL),
-(23, 'COBA SAJA', 22, NULL, 'Private', 7, '2025-11-04 01:40:37', '2025-11-04 01:40:37', NULL);
+INSERT INTO `folders` (`id`, `name`, `parent_id`, `divisi`, `status`, `created_by`, `created_at`, `updated_at`, `deleted_at`, `sort_order`) VALUES
+(17, 'Laravell', NULL, NULL, 'Private', 6, '2025-11-02 05:46:48', '2025-11-12 05:02:26', NULL, 0),
+(45, 'New folder (2)', NULL, NULL, 'Private', 6, '2025-11-12 05:15:11', '2025-11-12 05:26:37', NULL, 0),
+(46, 'New folder (3)', 17, NULL, 'Private', 6, '2025-11-12 05:34:15', '2025-11-12 05:34:38', NULL, 0),
+(47, 'New folder', 45, NULL, 'Private', 6, '2025-11-12 05:34:15', '2025-11-12 05:35:13', NULL, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2025_11_08_052554_create_notifications_table', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `notifications`
+--
+
+CREATE TABLE `notifications` (
+  `id` char(36) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `notifiable_type` varchar(255) NOT NULL,
+  `notifiable_id` bigint(20) UNSIGNED NOT NULL,
+  `data` text NOT NULL,
+  `read_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -135,12 +198,18 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `role`, `created_at`, `updated_at`, `photo`) VALUES
-(6, 'Admin', 'admin@gmail.com', '$2y$10$1u2iyMIauzrfyG94oN7PWOwjdI4CelSViQ9uUMimxRKzYo8YpYJZa', 'admin', '2025-10-17 20:55:07', '2025-11-04 05:24:11', 'profile_photos/6.jpg'),
+(6, 'Admin', 'admin@gmail.com', '$2y$10$1u2iyMIauzrfyG94oN7PWOwjdI4CelSViQ9uUMimxRKzYo8YpYJZa', 'admin', '2025-10-17 20:55:07', '2025-11-08 23:38:05', 'profile_photos/6.png'),
 (7, 'Diva', 'diva@gmail.com', '$2y$10$lWGRfAu/VtfkICobujE.3.sGdoURnVDD9UlI1GnSoo0XNnCJZQ8TC', 'editor', '2025-10-17 20:57:07', '2025-10-17 20:57:07', '');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indeks untuk tabel `favorites`
+--
+ALTER TABLE `favorites`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indeks untuk tabel `files`
@@ -175,6 +244,19 @@ ALTER TABLE `folders`
   ADD KEY `idx_parent_id` (`parent_id`);
 
 --
+-- Indeks untuk tabel `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `notifications`
+--
+ALTER TABLE `notifications`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `notifications_notifiable_type_notifiable_id_index` (`notifiable_type`,`notifiable_id`);
+
+--
 -- Indeks untuk tabel `users`
 --
 ALTER TABLE `users`
@@ -186,10 +268,16 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT untuk tabel `favorites`
+--
+ALTER TABLE `favorites`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
 -- AUTO_INCREMENT untuk tabel `files`
 --
 ALTER TABLE `files`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=103;
 
 --
 -- AUTO_INCREMENT untuk tabel `file_access_logs`
@@ -207,7 +295,13 @@ ALTER TABLE `file_shares`
 -- AUTO_INCREMENT untuk tabel `folders`
 --
 ALTER TABLE `folders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
+
+--
+-- AUTO_INCREMENT untuk tabel `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT untuk tabel `users`
