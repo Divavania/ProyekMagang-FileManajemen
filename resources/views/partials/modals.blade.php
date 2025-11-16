eeb8bf156f701657e9f91044f42393f24e44e0f9
Divavania <divavaniacandrawati@gmail.com>
2025-11-16 10:53:13 +0700

    UPDATE: modals upload file

--- resources/views/partials/modals.blade.php
+++ resources/views/partials/modals.blade.php
@@ -19,6 +19,25 @@
   </div>
 </div>
 
+<!-- Upload File Modal -->
+<div class="modal fade" id="uploadFileModal" tabindex="-1">
+  <div class="modal-dialog">
+    <form class="modal-content" method="POST" action="{{ route('store.file') }}" enctype="multipart/form-data">
+      @csrf
+      <div class="modal-header">
+        <h5>Upload File</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+      </div>
+      <div class="modal-body">
+        <input type="file" name="file" class="form-control" required>
+      </div>
+      <div class="modal-footer">
+        <button type="submit" class="btn btn-primary">Upload</button>
+      </div>
+    </form>
+  </div>
+</div>
+
 <!-- Upload Folder Modal (bisa upload folder biasa) -->
 <div class="modal fade" id="uploadFolderModal" tabindex="-1">
   <div class="modal-dialog">
